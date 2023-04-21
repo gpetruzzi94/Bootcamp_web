d58202fef04825d13fddba3e1970281682ec269f
gpetru94 <116055401+gpetruzzi94@users.noreply.github.com>
2023-04-21 00:30:11 -0300

    agrega la funcionalidad eliminar,agregar y editar

--- WebApplication2/Controllers/DepositosController.cs
+++ WebApplication2/Controllers/DepositosController.cs
@@ -6,11 +6,75 @@ namespace WebAppTarea11.Controllers
 {
     public class DepositosController : Controller
     {
+        DepositoServices depositoServices = new DepositoServices();
         public IActionResult Index()
         {
-            DepositoServices depositoServices = new DepositoServices();
+            
             List<Deposito> depositos= depositoServices.GetTodosLosDepositos();
             return View(depositos);
         }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Deposito articuloACrear = new Deposito();
+            return View(articuloACrear);
+
+
+        }
+        [HttpPost]
+        public IActionResult Create(Deposito depositoAGuardar)
+        {
+            string mensaje = depositoServices.AddDeposito(depositoAGuardar);
+            if (mensaje == "Deposito agregado")
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Mensaje = mensaje;
+                return View(depositoAGuardar);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Deposito depositoAEditar = depositoServices.GetDepositoPorId(id);
+            return View(depositoAEditar);
+
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Deposito depositoAEditar)
+        {
+            string mensaje = depositoServices.ModificarDeposito(depositoAEditar);
+            if (mensaje == "Deposito modificado")
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Mensaje = mensaje;
+                return View(depositoAEditar);
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            string mensaje = depositoServices.EliminarDeposito(id);
+            if (mensaje == "Deposito eliminado")
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Mensaje = mensaje;
+                return View();
+            }
+        }
+
+
+
     }
 }
