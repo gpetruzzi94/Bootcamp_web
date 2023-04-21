f7ee3a247aeb25a3ef06b0abe96a7c38242976c9
gpetru94 <116055401+gpetruzzi94@users.noreply.github.com>
2023-04-20 22:55:03 -0300

    Comienza a agregar input

--- WebApplication2/Controllers/ArticulosController.cs
+++ WebApplication2/Controllers/ArticulosController.cs
@@ -9,10 +9,24 @@ namespace WebAppTarea11.Controllers
     {
         public IActionResult Index()
         {
-            
+
             ArticuloServices articuloServices = new ArticuloServices();
             List<Articulo> articulos = articuloServices.GetAllArticulos();
             return View(articulos);
         }
+
+        
+        public IActionResult Create() { 
+            Articulo articuloACrear = new Articulo();
+            return View(articuloACrear);
+
+        
+        }
+        [HttpPost]
+        public IActionResult Create(Articulo articuloAGuardar)
+        {
+            return View();
+        }
+
     }
 }
